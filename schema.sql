a291706b9f55bdb1cf0b7f40ff5a077dadf69fcd
Bighem (US) <Caitlin.A.Bighem@boeing.com>
2022-07-15 20:02:54 -0500

    queries.sql

--- schema.sql
+++ schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -27,16 +27,16 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 );
 
 CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_emp (
-emp_no INT NOT NULL,
+	emp_no INT NOT NULL,
     dept_no VARCHAR(4) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -55,3 +55,13 @@ CREATE TABLE titles (
 );
 
 SELECT * FROM departments;
+
+SELECT * FROM employees;
+
+SELECT * FROM titles;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM salaries;
