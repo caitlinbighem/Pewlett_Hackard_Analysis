3935855600bbde393518f9b321f28eacd5988a4e
Bighem (US) <Caitlin.A.Bighem@boeing.com>
2022-07-18 14:27:37 -0500

    Module 7 Challenge Final Upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -8,8 +8,8 @@ SELECT e.emp_no,
 	t.to_date
 INTO retirement_titles
 FROM employees as e
-INNER JOIN titles as t
-ON (e.emp_no = t.emp_no)
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
@@ -28,3 +28,35 @@ ORDER BY emp_no, to_date DESC;
 
 --View unique_titles
 SELECT * FROM unique_titles;
+
+-- Retrieve the number of employees by their most recent job title who are about to retire.
+SELECT COUNT (ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut. title
+ORDER BY COUNT DESC;
+
+--View retiring_titles
+SELECT * FROM retiring_titles;
+
+-- DELIVERABLE 2: The Employees Eligible for the Mentorship Program
+-- Create membership_eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+-- View mentorship_eligibility table
+SELECT * FROM mentorship_eligibility;
